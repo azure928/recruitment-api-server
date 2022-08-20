fab8be0ce3b607240bec51027f8f4790fe79f0a4
azure928 <azure6.dev@gmail.com>
2022-08-20 21:02:28 +0900

    Add : 채용공고 수정 기능 구현

--- routes/job-posting.js
+++ routes/job-posting.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 // 채용공고 등록
 router.post('/jobpostings', jobServices.createJobPosting);
+// 채용공고 수정
+router.put('/jobpostings/:job_posting_id', jobServices.updateJobPosting);
 
 module.exports = router;
--- services/job-posting.js
+++ services/job-posting.js
@@ -20,4 +20,28 @@ const createJobPosting = async (req, res) => {
   }
 };
 
-module.exports = { createJobPosting };
+const updateJobPosting = async (req, res) => {
+  try {
+    const job_posting_id = req.params.job_posting_id;
+    const { position, compensation, content, skill } = req.body;
+    //console.log('req.prams 확인 : ', req.prams);
+    //console.log('req.body 확인 : ', req.body);
+
+    const result = await jobRepo.updateJobPosting(
+      job_posting_id,
+      position,
+      compensation,
+      content,
+      skill
+    );
+
+    console.log('result : ', result);
+
+    res.status(200).json({ message: 'SUCCESS' });
+  } catch (error) {
+    console.log(error.message);
+    res.status(error.statusCode || 500).json({ message: 'FAIL' });
+  }
+};
+
+module.exports = { createJobPosting, updateJobPosting };
