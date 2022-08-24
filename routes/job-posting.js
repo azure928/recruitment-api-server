0f4ef27fc08525e546e7cee63cf67aa09632650a
azure928 <azure6.dev@gmail.com>
2022-08-24 14:58:13 +0900

    Fix : 채용공고 수정 API - 메서드 변경 - 요청에 포함되어 있는 부분만 변경될 수 있도록 메서드를 PUT에서 PATCH로 변경함

--- routes/job-posting.js
+++ routes/job-posting.js
@@ -1,16 +1,19 @@
-const express = require('express');
-const jobServices = require('../services/job-posting');
+const express = require("express");
+const jobServices = require("../services/job-posting");
 const router = express.Router();
 
 // 채용공고 등록
-router.post('/jobpostings', jobServices.createJobPosting);
+router.post("/jobpostings", jobServices.createJobPosting);
 // 채용공고 수정
-router.put('/jobpostings/:job_posting_id', jobServices.updateJobPosting);
+router.patch("/jobpostings/:job_posting_id", jobServices.updateJobPosting);
 // 채용공고 삭제
-router.delete('/jobpostings/:job_posting_id', jobServices.deleteJobPosting);
+router.delete("/jobpostings/:job_posting_id", jobServices.deleteJobPosting);
 // 채용공고 목록 (검색)
-router.get('/jobpostings', jobServices.readJobPostings);
+router.get("/jobpostings", jobServices.readJobPostings);
 // 채용공고 상세 페이지
-router.get('/jobpostings/:job_posting_id', jobServices.readJobPostingDetailByPostingid);
+router.get(
+  "/jobpostings/:job_posting_id",
+  jobServices.readJobPostingDetailByPostingid
+);
 
 module.exports = router;
